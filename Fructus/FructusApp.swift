7372b031c3763237fb4b4b54d9e0640bf95b8c08
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-06-09 23:47:48 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Owen on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
